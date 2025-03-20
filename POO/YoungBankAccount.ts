512e2bc7d5514e07c88288821ed37e26d8c7455c
mahamadou <mahamadou.gory.sio@gmail.com>
2025-03-20 08:21:56 +0100

    Refactor YoungBankAccount transfer method to remove redundant access modifier

--- POO/YoungBankAccount.ts
+++ POO/YoungBankAccount.ts
@@ -6,7 +6,7 @@ export class YoungBankAccount extends BankAccount{
         console.log("!Vous ne pouvez pas retirer d'argent avec ce type de compte!\n");
     }
 
-    public override transfer(compte: BankAccount, somme: number): void {
+    override transfer(compte: BankAccount, somme: number): void {
         console.log("!Vous ne pouvez pas effectuer de transfert d'argent avec ce type de compte!\n");
     }
 }
