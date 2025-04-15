81b34b9866deecaadb5a7c9acc027403392a7f19
mahamadou <mahamadou.gory.sio@gmail.com>
2025-04-15 11:44:16 +0200

    feat: add initial Jest configuration for TypeScript testing

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.ts?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    testMatch: ['**/test/**/*.test.ts'],
+  };
+  
